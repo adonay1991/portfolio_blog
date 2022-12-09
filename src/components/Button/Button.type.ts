a77e964e3b7bea433c5fce30484d49603d6d5d5e
Adonay Rodriguez <aguilarte@atlax360.com>
2022-12-10 00:38:40 +0100

    docs(TSdocs): :zap: added docs to types

--- src/components/Button/Button.type.ts
+++ src/components/Button/Button.type.ts
@@ -1,5 +1,14 @@
 import type { JSXElement } from "solid-js";
 
+/**
+ * `ButtonProps` is an object that has an optional isDisabled property of type boolean, an optional
+ * children property of type JSXElement, an optional type property of type 'submit' | 'reset', and an
+ * optional id property of type string.
+ * `isDisabled` - A boolean that determines whether the button is disabled or not.
+ * `children` - The content of the button.
+ * `type` - The type of button.
+ * `id` - The id of the button.
+ */
 export type ButtonProps = {
 	isDisabled?: boolean;
 	children?: JSXElement;
--- src/components/CardPreview/CardPreview.types.ts
+++ src/components/CardPreview/CardPreview.types.ts
@@ -1,7 +1,20 @@
+/**
+ * @remarks
+ * `CardProps` is an object with a bunch of properties, some of which are required and some of which
+ * are optional.
+ * `url` - The url of the article
+ * `img` - The image to be displayed on the card.
+ * `title` - The title of the card
+ * `author` - The author of the article.
+ * `footer` - The footer of the card.
+ * `date` - The date the article was published
+ * `description` - The description of the card.
+ * `tags` - string[];
+ */
 export type CardProps = {
 	url: string;
 	img: string;
-	title: string;
+	title?: string;
 	author?: string;
 	footer?: string;
 	date: string;
